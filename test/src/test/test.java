c834de9c46b7fea2ab5d454314476245a655ab3b
riteshjain1307 <50409149+riteshjain1307@users.noreply.github.com>
2019-05-08 18:42:35 +0530

    Add files via upload

---
+++ test/src/test/test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		boolean s=false;
+		// TODO Auto-generated method stub
+		while(s){
+			System.out.println("test");
+		}
+	
+
+	}
+
+}
