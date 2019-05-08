9e25374111f30ffba523587afc3b17b64a4d46e2
riteshjain1307 <50409149+riteshjain1307@users.noreply.github.com>
2019-05-08 18:54:45 +0530

    Add files via upload

---
+++ HelloJenkins.java
@@ -0,0 +1,17 @@
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Jenkins");
+		System.out.println("Hello Jenkins...Svn Commit");
+		
+		System.out.println("Hello Jenkins...Svn Commit....jenkins commit");
+		
+		System.out.println("Hello Jenkins...Svn Commit....jenkins commit After Changes");
+		
+		
+
+	}
+
+}
